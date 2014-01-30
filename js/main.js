53afcd40d9235da91be0b64ddb443c915945dfaa
bryan kennedy <bkennedy@smm.org>
2014-01-30 17:10:19 -0600

    Clickable boxes
    This gets us clickable divs instead of just a hrefs. The colors are
    wrong right now, but those'll change in a bit.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(".box1").click(function(){
+    window.location=$(this).find("a").attr("href");
+    return false;
+});
 
+// Clickable boxes
+$('.box1 p').addClass('box-resting');
+$(".box1").mousedown(function(){
+    console.log("down");
+    $(this).children('p').addClass('box-active');
+});
+$(".box1").mouseup(function(){
+    console.log("up");
+    $(this).children('p').removeClass('box-active');
+    $(this).children('p').addClass('box-resting');
+});
