43da9fae83ae905bed58c82ae5ad451098a6088c
bryan kennedy <bkennedy@smm.org>
2014-01-31 12:12:55 -0600

    Fixing click jquery after class name change

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-$(".box1").click(function(){
+$(".box").click(function(){
     window.location=$(this).find("a").attr("href");
     return false;
 });
 
 // Clickable boxes
-$('.box1 p').addClass('box-resting');
-$(".box1").mousedown(function(){
+$('.box p').addClass('box-resting');
+
+$(".box").mousedown(function(){
     console.log("down");
     $(this).children('p').addClass('box-active');
 });
-$(".box1").mouseup(function(){
-    console.log("up");
+
+$(".box").mouseup(function(){
     $(this).children('p').removeClass('box-active');
     $(this).children('p').addClass('box-resting');
 });
