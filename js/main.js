dafabc76f0ae224a4d6204e6dd65350685666e28
bryan kennedy <bkennedy@smm.org>
2014-02-21 16:38:40 -0600

    Refactoring screensaver code
    Removing unused code
    Removing duplicate code

--- js/main.js
+++ js/main.js
@@ -14,88 +14,52 @@ function getUrlVar(key){
 //
 var s_saver;
 // Debug screensaver length
-var s_saver_timeout = 2000;
+var s_saver_timeout = 5000;
 //var s_saver_timeout = 360000;
 
 // Page attributes
 var current_page = $(location).attr('pathname');
+console.log(current_page);
 var s_saver_enabled = getUrlVar('saver');
 
 //
-// Check screensaver states on ever page load
+// Screensaver - load
 //
-$( document ).ready(function() {
+// Check if the URL tells us to load the screensaver.
+// This happens when the timeout occurs on a sub page. The user is sent
+// back to the index page with this URL flag, enabling the screensaver.
+//
+$(window).bind("load", function() {
 
-    // Check if the URL tells us to load the screensaver.
-    // This happens when the timeout occurs on a sub page. The user is sent
-    // back to the index page with this URL flag, enabling the screensaver.
     if (s_saver_enabled != 'true') {
         // Start by hiding the screensaver overlay
         $('#screensaver').hide();
-
-        // Play the video
-        // Disabling this for testing
-        //$('#intro-overlay').show();
 
         // Run a timer from the load of the page
         s_saver = setTimeout(function(){
-            // If we're not on the homepage, go back there and start the
-            // screen saver.
-            if (current_page != 'index.html') {
-                window.location.replace('index.html?saver=true');
-            }
-            // If we're on the homepage, just start the screensaver.
-            else {
-                $('#screensaver').fadeIn(900);
-            }
+            window.location.replace('index.html?saver=true');
         }, s_saver_timeout);
     }
+
 });
 
-$(window).bind("load", function() {
-    if (current_page == 'index.html') {
-        $('#intro-overlay').hide();
-    }
-    if (s_saver_enabled != 'true') {
-        // Start by hiding the screensaver overlay
-        $('#screensaver').hide();
-
-        // Play the intro video
-        $('#intro-overlay').show();
-        $("#intro-overlay-video")[0].player.play();
-
-    }
-});
-
-
+//
+// Screensaver - reset
 //
 // Any time we get a click, reset the screensaver timer and make sure the
 // screensaver is cleared.
 //
 $('body').mousedown(function() {
-    clearTimeout(s_saver);
 
-    // Start the screensaver timer after a click
-    s_saver = setTimeout(function(){
-        // TODO - don't duplicate this form above. Make a function.
-        if (current_page != 'index.html') {
-            window.location.replace('index.html?saver=true');
-        }
-        else {
-            $('#screensaver').fadeIn(900);
-        }
-    }, s_saver_timeout);
+    // Hide screensaver
     $('#screensaver').fadeOut(100);
 
-    //if (current_page != 'index.html') {
-        //$('#intro-overlay').fadeIn(300);
-        //$("#intro-overlay-video")[0].player.play();
-    //}
-});
+    // Reset the screensaver timer
+    clearTimeout(s_saver);
+    s_saver = setTimeout(function(){
+        window.location.replace('index.html?saver=true');
+    }, s_saver_timeout);
 
-$('#intro-overlay').mousedown(function() {
-    $("#intro-overlay-video")[0].player.pause();
-    $('#intro-overlay').fadeOut(300);
 });
 
 $(".box").click(function(){
